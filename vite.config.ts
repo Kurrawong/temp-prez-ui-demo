9b0520fadeaff767578819c83120dab3a2227818
recalcitrantsupplant <dcchabgood@gmail.com>
2023-02-22 17:23:09 +1000

    Updates for prez demo deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/temp-prez-ui-demo/",
     plugins: [vue()],
     resolve: {
         alias: {
