9ec1f2370faf67d8be89945862b9536a546d26a2
david <dcchabgood@gmail.com>
2023-03-16 10:39:18 +1000

    tempdemo -> main

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
-#then run npm run build && \
+#then run
+#npm run build && \
 git add dist -f && \
 git commit -m "Adding dist" && \
 git push origin main && \
-git -c credential.helper= -c core.quotepath=false -c log.showSignature=false push --progress --porcelain tempdemo :gh-pages && \
-git subtree push --prefix dist tempdemo gh-pages
+git -c credential.helper= -c core.quotepath=false -c log.showSignature=false push --progress --porcelain main :gh-pages && \
+git subtree push --prefix dist main gh-pages
