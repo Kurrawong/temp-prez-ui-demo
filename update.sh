ad609bc3339148e186db15b4ad321bd1017c0101
david <dcchabgood@gmail.com>
2023-03-16 10:36:09 +1000

    change origin

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #then run npm run build && \
 git add dist -f && \
 git commit -m "Adding dist" && \
-git push tempdemo main && \
+git push origin main && \
 git -c credential.helper= -c core.quotepath=false -c log.showSignature=false push --progress --porcelain tempdemo :gh-pages && \
 git subtree push --prefix dist tempdemo gh-pages
